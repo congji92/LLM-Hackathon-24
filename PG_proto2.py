23a60e278168441b5351def832cc600697fdd4b3
ColiJun <licongji92@gmail.com>
2024-12-06 13:45:46 -0800

    Update PG_proto2.py

--- PG_proto2.py
+++ PG_proto2.py
@@ -15,45 +15,51 @@ from maps_text_search import Agent_search
 
 ### LLM
 
-llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key = 'type_your_own' ) 
+llm = ChatOpenAI(model="gpt-4o", temperature=0, api_key = 'OpenAI API Here' ) 
 
 ### Schema 
 
 class OrderState(TypedDict):
     """State representing the customer's order conversation."""
     messages: Annotated[list, add_messages] # aka the MessagesState class
-    destination: str
-    duration: str
+    destination: str                  #TBD
+    duration: str                           #TBD
     order: list[str]
     suggestions: list[str]
-    finished: bool
-    ready_to_search: bool
+    selections: list[str]
+    finished: bool                          #TBD
+    desination_collected: bool
+    pref_collected: bool
+    search_returned: bool
+    selected: bool
+    plan_ordered: bool
 
 TravelBOT_SYSINT = (
     "system",  # 'system' indicates the message is a system instruction.
     "You are a TravelBot, an interactive travel planning system. You will do the following:"
-    
-    "Step 1: A customer will talk to you about the preferences for destination, places of interests, food, and etc, "
-    "you will gather preferences, usually at lease one destination,"
-    "and you will answer any questions about travel preferences (and only about travel preferences - no off-topic discussion). "
-    "Add destination, places of interest and food preferences to the customer's order by calling add_to_order"
-    "To see the contents of the order so far, call get_order (this is shown to you, not the user) "
-    "Customer may request to reset the order by calling clear_order. "
-    "The customer will confirm the collected preferences, which you will structure."
-    
-    "Step 2: Search preferences by calling get_search. "
-    "Always verify and respond with places of interest and restaurant from the get_search before adding them to the suggestions. "
-    "You only have the places and restaurants listed by calling get_search. "
 
-    "Step 3: Once having called get_search, call confirm_order to ensure it is correct, then make "
-    "any necessary updates and then call plan_order. "
-    "Always confirm_order with the user (double-check) before calling plan_order. Calling confirm_order will "
-    "display the preferences and suggestions to the user and returns their response to seeing the list. Their response may contain modifications. "
+    "Step 1: A customer will talk to you about the preferences for destination. Add destination by calling add_to_destination."
+    "You will answer any questions about destinations (and only about destinations - no off-topic discussion). "
+    
+    "Step 2: The customer will talk to you about the preferences for places of interests or food. "
+    "Add those preferences to the customer's order by calling add_to_order"
+    "You will answer any questions about preferences (and only about preferences - no off-topic discussion). "
 
-    "Step 4: If user confirmes, thank the user and say goodbye!",
+    "Step 3: Search preferences by calling get_search. "
+    "You only have the places and restaurants listed in suggestions by calling get_search. "
+
+    "Step 4: You will call confirm_order in order to ask for customer's choice from the suggestions. "
+
+    "Step 5: Customer might select a part of or all suggestions, call select_order to record customer selections. "
+    "Customer might request to make a new search, which will have the system go back to step 1 and follow the same step order. "
+
+    "Step 6: After user makes a selection, call plan_order."
+    "Customer might request to make a new search, which will have the system go back to step 1 and follow the same step order. "
+
+    "Step 7: If user confirmes the plan, then thank the user and say goodbye!",
 )
 
-WELCOME_MSG = "Welcome to DJ (Dr.J), the travel agent. Type `q` to quit. What's on your mind for travel today?"
+WELCOME_MSG = "Welcome to DJ, the travel agent. What's on your mind today? Type `q` to quit"
 
 def human_node(state: OrderState) -> OrderState:
     print("Model:", state["messages"][-1].content)
@@ -84,13 +90,18 @@ def search_node(state: OrderState) -> OrderState:
     tool_msg = state.get("messages", [])[-1]
     order = state.get("order", [])
     outbound_msgs = []
-    order_placed = False
+    destination = state.get("destination", '')
+    destination_collected = state.get("destination_collected", False)
+    pref_collected = state.get("pref_collected", False)
+    search_returned = state.get("search_returned", False)
+    suggestions = state.get("order", [])
 
     for tool_call in tool_msg.tool_calls:
-        if tool_call["name"] == "get_search":
-            print('debug step 1: ', order)
-            query = order[0]
-            agent = Agent_search(query, 37.802516, -122.399833, 37.775271, -122.431116)
+        if tool_call["name"] == "get_search" and pref_collected:
+            print(f"Debuge step 3: call api according to destination: {destination}, order (preferences): {order}")
+            query = ''.join(order)
+            print('debug step 2.1: query is: ', query)
+            agent = Agent_search(destination, query)
             result = agent.search().tolist()
             response = "\n found the following: ".join(result)
 
@@ -98,9 +109,9 @@ def search_node(state: OrderState) -> OrderState:
                 content=response,
                 name=tool_call["name"],
                 tool_call_id=tool_call["id"],))
-        suggestion = result
-        return state | {"messages": outbound_msgs, "suggestions": suggestion }
-
+        suggestions = result
+        search_returned = True
+        return state | {"messages": outbound_msgs, "suggestions": suggestions, "search_returned": search_returned }
 
 from langgraph.prebuilt import ToolNode
 from typing import Literal
@@ -118,6 +129,13 @@ from random import randint
 from langgraph.prebuilt import InjectedState
 from langchain_core.messages.tool import ToolMessage
 @tool
+def add_to_destination(location: str) -> str:
+    """Adds the specified destination.
+    Returns:
+      The updated destination.
+    """
+
+@tool
 def add_to_order(requests: str) -> str:
     """Adds the specified preferences for place of interest and food.
     Returns:
@@ -126,7 +144,7 @@ def add_to_order(requests: str) -> str:
 
 @tool
 def confirm_order() -> str:
-    """Asks the customer if the preference is correct.
+    """Asks for selection.
     Returns:
       The user's free-text response.
     """
@@ -140,6 +158,13 @@ def clear_order():
     """Removes all items from the user's order."""
 
 @tool
+def select_order(selection: str) -> str:
+    """Adds the specified suggestions into selections.
+    Returns:
+      The updated selections in progress.
+    """
+
+@tool
 def plan_order() -> int:
     """Sends the food and place suggestions for planning.
 
@@ -150,31 +175,53 @@ def plan_order() -> int:
 def order_node(state: OrderState) -> OrderState:
     """The ordering node. This is where the order state is manipulated."""
     tool_msg = state.get("messages", [])[-1]
+    destination = state.get("destination", '')
     order = state.get("order", [])
     suggestions = state.get("suggestions", [])
+    selections = state.get("selections", [])
     outbound_msgs = []
-    order_placed = False
+    destination_collected = state.get("destination_collected", False)
+    pref_collected = state.get("pref_collected", False)
+    search_returned = state.get("search_returned", False)
+    selected = state.get("selected", False)
+    plan_ordered = state.get("plan_ordered", False)
 
     for tool_call in tool_msg.tool_calls:
-        if tool_call["name"] == "add_to_order":
+        if tool_call["name"] == "add_to_destination":
+            destination = (f'{tool_call["args"]["location"]}')
+            print('debug step 1: updating destination: ', destination)
+            response = "\n".join(destination)
+            destination_collected = True
+
+        elif tool_call["name"] == "add_to_order":
+            order = []
             order.append(f'{tool_call["args"]["requests"]}')
-            print('debug step 1: updating preferences to order: ', order)
+            print('debug step 2: updating preferences to order: ', order)
             response = "\n".join(order)
+            pref_collected = True
 
         elif tool_call["name"] == "confirm_order":
-            print("Confirming tool_call: \nYour preferences:")
+            print("debug step 4: Confirming suggestions: \nYour destination:")
+            if not destination:
+                print("  (no destination supplied by customer)")
+            elif destination:
+                print(f"  {destination}")
+
+            print("\nYour preferences:")
             if not order:
-                print("  (no preferences)")
+                print("  (no preferences supplied by customer)")
             elif order:
               for items in order:
                 print(f"  {items}")
+
+            print(" \nSuggestions for you: \n")
             if not suggestions:
-                print("  (no suggestions)")
+                print("  (no suggestions provided by search)")
             elif order:
               for items in suggestions:
                 print(f"  {items}")
 
-            response = input("Is this correct? ")
+            response = input("Any suggestions that looks interesting to you? ")
 
         elif tool_call["name"] == "get_order":
 
@@ -185,14 +232,28 @@ def order_node(state: OrderState) -> OrderState:
             order.clear()
             response = None
 
-        elif tool_call["name"] == "plan_order":
+        elif tool_call["name"] == "select_order":
+            selections.append(f'{tool_call["args"]["selection"]}')
+            print('debug step 5: selections to plan: ', selections)
+            response = "\n".join(selections)
+            selected = True
+            order = []
+            destination = ''
+
+        elif tool_call["name"] == "plan_order" and selected == True:
 
             order_text = "\n".join(suggestions)
-            print("\n Sending place and food suggestions to planner!\n")
-            print('Debug step 3: call plan_order to send to planning: ', order_text)
+            print('Debug step 6: call plan_order with following selections: ', selections)
+            plan_ordered = True
+            destination_collected = False
+            pref_collected = False
+            search_returned = False
+            selected = False
+            order = []
+            destination = ''
+            suggestions = []
 
             # TODO: Implement planning based on order.
-            order_placed = True
             response = randint(1, 5)  # ETA in minutes
 
         else:
@@ -206,7 +267,7 @@ def order_node(state: OrderState) -> OrderState:
                 tool_call_id=tool_call["id"],
             )
         )
-    return state | {"messages": outbound_msgs, "order": order, "ready_to_search": order_placed}
+    return state | {"messages": outbound_msgs, "destination": destination, "order": order, "suggestions": suggestions, "destination_collected": destination_collected, "pref_collected": pref_collected, "selected": selected, "selections": selections, "plan_ordered": plan_ordered}
 
 def maybe_route_to_tools(state: OrderState) -> str:
     if not (msgs := state.get("messages", [])):
@@ -222,7 +283,7 @@ def maybe_route_to_tools(state: OrderState) -> str:
     else:return "human"
 
 auto_tools = [get_search]
-order_tools = [add_to_order, confirm_order, get_order, clear_order, plan_order]
+order_tools = [add_to_destination, add_to_order, confirm_order, get_order, clear_order, select_order, plan_order]
 llm_with_tools = llm.bind_tools(auto_tools + order_tools)
 
 graph_builder = StateGraph(OrderState)
